460566fbcb3a52be154e3d1ea1cad73c764f55fa
YCRUZ1 <90659305+YCRUZ1@users.noreply.github.com>
2021-10-06 22:31:57 -0500

    Se agrego nada mas a la carpeta src

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import login from './Images/login.svg';
+//import login from './Images/login.svg';
 
 import './App.css';
 
@@ -7,27 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
       </header>
-
-      <body>  
-      <div className="login-container">
-        <div className="login-info-container">
-            <h1 class="title">Login</h1>
-            <div class="social-login">
-                <div class="social-login-element">
-                    <imgen src={"Images/google.svg"} alt="google-image"/>
-                    <span>Google</span>
-                </div>
-            </div>
-            <form class="inputs-container">
-                <p>Olvidaste la Contraseña? <span class="span">Click here</span></p>
-                <button class="btn">login</button>
-                <p>No tienes cuenta? <span class="span">Sign Up</span></p>
-            </form>
-        </div>
-        <img class="image-container" src={login} alt="IMAGEN"/>
-    </div>
-    </body>
-    </div>
+      <body>
+  
+</body>
+</div>
   );
 }
 
