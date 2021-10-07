17a39be983b4d2720bd044056c919c440f99117a
YCRUZ1 <90659305+YCRUZ1@users.noreply.github.com>
2021-10-06 21:49:01 -0500

    se cambio todo a app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import login from './Images/login.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <body>  
+      <div className="login-container">
+        <div className="login-info-container">
+            <h1 class="title">Login</h1>
+            <div class="social-login">
+                <div class="social-login-element">
+                    <imgen src={"Images/google.svg"} alt="google-image"/>
+                    <span>Google</span>
+                </div>
+            </div>
+            <form class="inputs-container">
+                <p>Olvidaste la Contraseña? <span class="span">Click here</span></p>
+                <button class="btn">login</button>
+                <p>No tienes cuenta? <span class="span">Sign Up</span></p>
+            </form>
+        </div>
+        <img class="image-container" src={login} alt="IMAGEN"/>
+    </div>
+    </body>
     </div>
   );
 }
